2ff5ae4e29856bcb759043fac36ca51e81314080
Sangeetha <sangeethapa2002@gmail.com>
2023-11-25 19:21:22 +0530

    Added ATM Process

---
+++ Table Pattern.py
@@ -0,0 +1,20 @@
+
+print("Account_Number", "\t", "Account_Holder", "\t", "Pin_Number", "\t", "Balance",  "\n")
+
+for i in range(5):
+    number = i + 101
+    tab = "             "
+    if number == 101:
+        print(" ", number, tab, "Suresh", tab, " ", 2343, "\t", 25432, "\n")
+    elif number == 102:
+        print(" ", number, tab, "Ramesh", tab, " ", 5432, "\t", 34532, "\n")
+    elif number == 103:
+        print(" ", number, tab, "Ganesh", tab, " ", 7890, "\t", 54980, "\n")
+    elif number == 104:
+        print(" ", number, tab, "Magesh", tab, " ", 2650, "\t", 80000, "\n")
+    elif number == 105:
+        print(" ", number, tab, "Naresh", tab, " ", 2343, "\t", 100000, "\n")            
+
+    
+    
+ 
